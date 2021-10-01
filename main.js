8d7a560f4ec3637d65c6c4bbdf092beb9af82b12
Rio Kinslow <enna_dj@yahoo.com>
2021-09-30 20:15:55 -0400

    Advanced Counter Demo

--- main.js
+++ main.js
@@ -19,39 +19,112 @@
   stopTimer(): stop the timer, then render()
 */
 
-const countView = document.querySelector("#count")
-let count = 0
+/* 
+Object- Oriented Programming:
+ -Objects & Methods: Bundle date and related functions together (encapsulation).
+ -Polumorphism:...
 
-function render () {
-  countView.replaceChildren(count)
-  console.log(count)
+ "Method": a function which is a property on an object.
+ "this": is the context in which the function was cALLED - not necessarily
+ the context in which it was written.
+
+ counter.next()
+
+*/
+const counter = {
+  count: 0,
+  countView: document.querySelector("#count"),
+  timerId: null, 
+
+  render: function render () {
+    this.countView.replaceChildren(this.count)
+    console.log(this.count)
+  },
+
+  next: function next () {
+    this.count += 1
+    this.render()
+  },
+
+  prev: function prev () {
+    this.count -= 1
+    this.render()
+  },
+  
+  reset: function reset () {
+    this.count = 0
+    this.render()
+  },
+  
+  skipForward: function skipForward () {
+    this.count += 10
+    this.render()
+  },
+  
+  skipBackward: function skipBackward () {
+    this.count -= 10
+    this.render()
+
+  },
+
+  startTimer: function startTimer () {
+    this.timerId = window.setInterval(() => this.next(), 1000)
+  },
+
+  stopTimer: function stopTimer () {
+    window.clearInterval(this.timerId)
+  },
+
+
+ }
+
+const newsletterCounter = {
+  count: 500,
+  countView: document.querySelector("#newsletter-count"),
+
+  render: counter.render,
+  next: counter.next,
+  prev: counter.prev,
+  reset: counter.reset,
+  skipForward: counter.skipForward,
+  skipBackward: counter.skipBackward,
 }
 
-function next () {
-  count += 1
-  render()
-}
 
-function prev () {
-  count -= 1
-  render()
-}
+document
+  .querySelector("#controls")
+  .addEventListener("click", event => {
+    const clickedElement = event.target
 
-function reset () {
-  count = 0
-  render()
-}
+    switch (clickedElement.id) {
+      case "next": counter.next()
+      break
 
-function skipForward () {
-  count += 10
-  render()
-}
+      case "prev": counter.prev()
+      break
 
-function skipBackward () {
-  count -= 10
-  render()
-}
+      case "reset": counter.reset()
+      break
 
+      case "skip-forward": counter.skipForward()
+      break
+
+      case "skip-backward": counter.skipBackward()
+      break
+
+      case "start-timer": counter.startTimer()
+      break
+
+      case "stop-timer": counter.stopTimer()
+      break
+    }
+  })
+
+
+    
+    
+
+  
 
 
 
